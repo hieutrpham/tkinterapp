de78607b9333c66d15ab0af09e5662ec561e517d
hieutrpham <37964079+hieutrpham@users.noreply.github.com>
2019-05-13 22:32:19 -0400

    add login tk

---
+++ login.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import ttk
+
+class Login:
+
+    def __init__(self, master):
+
+        master.title('Login')
+        master.resizable(False, False)
+
+        self.frame = ttk.Frame(master)
+        self.frame.pack()
+
+        self.label_user = ttk.Label(self.frame, text='Username:')
+        self.label_user.grid(row=0, column=0)
+
+        self.label_pass = ttk.Label(self.frame, text='Password:')
+        self.label_pass.grid(row=1, column=0)
+
+        self.entry_user = ttk.Entry(self.frame)
+        self.entry_user.grid(row=0, column=1)
+
+        self.entry_pass = ttk.Entry(self.frame)
+        self.entry_pass.grid(row=1, column=1)
+
+        self.button_submit = ttk.Button(self.frame, text='Submit', command=self.submit)
+        self.button_submit.grid(row=2, column=0)
+
+        self.button_cancel = ttk.Button(self.frame, text='Cancel', command=self.cancel)
+        self.button_cancel.grid(row=2, column=1)
+
+    def submit(self):
+        print('submited')
+
+    def cancel(self):
+        pass
+
+def main():
+    root = tk.Tk()
+    Login(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
